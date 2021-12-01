87a04124faeb36c4b2b575588f3929e39eba9ee7
sindhupaila <95371195+sindhupaila@users.noreply.github.com>
2021-12-01 22:43:50 +0530

    Update 04_sum_of_squares.py

--- 04_sum_of_squares.py
+++ 04_sum_of_squares.py
@@ -4,7 +4,13 @@ question_04 = ''' Python script that takes a positive integer n and returns the 
 
 
 def sum_squares(n):
-  pass
+  temp=0
+  if n>1:
+    for i in range(1,n):
+      temp = temp + i**2
+    return temp
+  else:
+    return 0
         
 
 
@@ -24,5 +30,5 @@ class TestSumSquares(unittest.TestCase):
   def test_05(self):
     self.assertEqual(sum_squares(100), 328350)
   
-if __name__ == '__main__':
+if _name_ == '_main_':
     unittest.main(verbosity=2)
